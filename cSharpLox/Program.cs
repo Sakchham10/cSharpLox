41553df9923c913642ced28a80bb41dcd2cbe583
Sakchham10 <Sakchham10@Hotmail.com>
2024-06-03 20:01:15 -0500

    First commit

---
+++ cSharpLox/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+public class Lox
+{
+    static Boolean hadError = false;
+    public static void main(String[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: jlox [script]");
+            Environment.Exit(64);
+        }
+        else if (args.Length == 1)
+        {
+            runFile(args[0]);
+        }
+        else
+        {
+            runPrompt();
+        }
+    }
+
+    public static void runFile(String path)
+    {
+        byte[] bytes = File.ReadAllBytes(path);
+        String content = Encoding.Default.GetString(bytes);
+        run(content);
+        if (hadError == true) Environment.Exit(65);
+    }
+
+    private static void runPrompt()
+    {
+        for (; ; )
+        {
+            Console.Write(">");
+            var line = Console.ReadLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source)
+    {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.ReadTokens();
+        tokens.ForEach((token) => Console.WriteLine(token));
+    }
+
+    static void error(int line, String message)
+    {
+        report(line, "", message);
+    }
+
+    private static void report(int line, string where, string message)
+    {
+        Console.Error.WriteLine("[line" + line + "] Error" + where + " : " + message);
+        hadError = true;
+    }
+}
+
