6d4bbe7a70197e9050cedc80b316643146195459
Sakchham10 <Sakchham10@Hotmail.com>
2024-06-10 22:30:15 -0500

    Created script for generating expression files

--- cSharpLox/tools/GenerateAst.cs
+++ cSharpLox/tools/GenerateAst.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.IO;
+using System;
 namespace interpreter.tools
 {
     public class GenerateAst
@@ -10,6 +13,73 @@ namespace interpreter.tools
                 Environment.Exit(65);
             }
             string outputDir = args[0];
+            defineAst(outputDir, "Expr", new List<string>{
+                    "Binary : Expr left, Token Operator, Expr right",
+                    "Grouping : Expr expression",
+                    "Literal : Object value",
+                    "Unary : Token Operator, Expr right",
+                    });
+
+        }
+
+        private static void defineAst(string outputDir, string baseName, List<string> types)
+        {
+            string path = outputDir + "/" + baseName + ".cs";
+            StreamWriter writer = new StreamWriter(path, false);
+
+            writer.WriteLine("namespace interpreter.lox ;");
+            writer.WriteLine();
+            writer.WriteLine();
+            writer.WriteLine("public abstract class " + baseName + " {");
+            writer.WriteLine("}");
+            foreach (string type in types)
+            {
+                string className = type.Split(":")[0].Trim();
+                string fields = type.Split(":")[1].Trim();
+                defineType(writer, baseName, className, fields);
+            }
+            writer.WriteLine("}");
+            writer.Close();
+        }
+
+        private static void defineType(StreamWriter writer, string baseName, string className, string fieldList)
+        {
+            writer.WriteLine("  public class " + className + " : " + baseName + " {");
+
+            // Constructor.
+            writer.WriteLine("  private " + className + "(" + fieldList + ") {");
+
+            // Store parameters in fields.
+            string[] fields = fieldList.Split(", ");
+            foreach (string field in fields)
+            {
+                string name = field.Split(" ")[1];
+                writer.WriteLine("_" + name + " = " + name + ";");
+            }
+
+            writer.WriteLine("    }");
+
+            // Fields.
+            writer.WriteLine();
+
+            writer.WriteLine("   public static " + className + " Create" + "(" + fieldList + ") {");
+            string fieldNames = new string("");
+            foreach (string field in fields)
+            {
+                string name = field.Split(" ")[1];
+                fieldNames = String.Concat(fieldNames, name, ",");
+            }
+            fieldNames = fieldNames.Remove(fieldNames.Length - 1);
+            writer.WriteLine("   return new " + className + "(" + fieldNames + ") ;}");
+            foreach (string field in fields)
+            {
+                string type = field.Split(" ")[0];
+                string name = field.Split(" ")[1];
+                writer.WriteLine("    static " + type + " " + "_" + name + ";");
+            }
+
+            writer.WriteLine("  }");
+
         }
     }
 }
